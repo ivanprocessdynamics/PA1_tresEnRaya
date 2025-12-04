2b0139000b66fd3054e0742ff85b32f7fee1b096
cezar <cezarvalentinivan@gmail.com>
2025-12-04 12:27:05 +0100

    Cezar versión 1

---
+++ abs_board.py
@@ -0,0 +1,164 @@
+from constants import PLAYER_COLOR, BSIZ, NO_PLAYER, ST_PLAYER
+
+from collections import namedtuple
+
+# Representación de las piedras en el tablero.
+# Utilizamos de coordenadas x, y (fila y columna)
+# Utilizamos el índice del jugador (0/1), que se usa con PLAYER_COLOR['color']
+
+Stone = namedtuple('Stone', ('x', 'y', 'color'))
+
+
+def set_board_up(stones_per_player = ST_PLAYER): 
+
+    #Inicialización del tablero
+    #Vamos a hacer una matriz donde NO_PLAYER = -1 significa casilla no jugada, 0 es cassilla
+    #del jugador 0 y 1 es casilla del jugador 1
+
+    i, j = 0                                       #contador filas y columnas
+    board = []                                     #esto será la matriz
+
+    while i < BSIZ:
+        fila = []
+        while j < BSIZ:
+            fila.append(NO_PLAYER)                 #Añadimos NO_PLAYER en fila BSIZ veces
+            j += 1
+
+        board.append(fila)                         #Ponemos la fila de NO_PLAYER [-1, -1, -1] en el tablero, BSIZ veces
+        i += 1
+        j = 0                                      #reiniciamos el contador de columnas a cero
+
+
+
+    # Ahora toca definir las distintas variables involucradas en el tablero
+
+    # 1. Lista de piedras: cada elemento será Stone(x, y, color)
+    stones_list = []
+
+    # 2. Jugador actual: 0 o 1
+    current_player = 0
+
+    # 3. Fase del juego: Placing (se colocan las piedras) o Moving (todas colocadas, ahora se mueven)
+    phase = 'placing'
+
+    # 4. Número de piedras colocadas por cada jugador. El primer valor corresponde al jugador 0 y el segundo al 1
+    stones_placed = [0,0]
+
+    # 5. Índice de piedra seleccionado en la fase moving. Se refiere al índice de stones_list
+    selected_index = None
+
+
+
+    #---------------------------------#
+    #            FUNCIONES            #
+    #---------------------------------#
+
+    # Solo  nos dice que devuelve las piedras que hay
+    def stones():
+        return stones_list
+
+    # Dibujar el tablero. Se puede hacer con dos while, yendo valor por valor comprobando si es igual a
+    # -1, 0 o 1, y en función a eso imprimir ".". "X" o "O"
+    def draw_txt(end = False):
+
+        fila, col = 0, 0
+
+        while fila < BSIZ:
+            linea = ""                             # Se imprimirá más tarde. Se inicializa ahora porque se le añadirán cosas
+
+            while col < BSIZ:
+                posicion = board[fila][col]        #Recorremos toda la matriz
+                
+                if posicion == NO_PLAYER:          #Dependiendo del valor que hay en cada posición, ponemos X, O, ".
+                    caracter = "."
+                elif posicion == 0:
+                    caracter = 'O'
+                else:
+                    caracter = 'X'
+
+                linea = linea + caracter + ' '     # Linea pasa a ser cada caracter de la fila, con espacios ' ' para que se vea mejor
+                col += 1
+            
+            print(linea)                           # Se imprime una fila y se pasa a la siguiente
+            fila += 1
+            col = 0
+
+        if not end:                                                  # Si end es False, la partida continúa (y mostramos a quién le toca). Si es True se acaba.
+            print("Turno del jugador:", current_player)              # El valor de end es recibido desde la función move_st(), del fichero main_txt.py
+        else:
+            print("Fin de la partida")
+
+
+    # Devuelve True si el jugador 'player' ha hecho 3 en ralla. Le pasamos 'player' desde move_st
+    def end(player):
+
+        # Comprobar horizontal
+
+        fila = 0
+        while fila < BSIZ:
+            if board[fila][0] == player and board [fila][1] == player and board[fila][2] == player:
+                return True
+            fila += 1
+        
+        # Comprobar vertical
+
+        col = 0
+        while col < BSIZ:
+            if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+                return True
+            col += 1
+
+        # Comprobar las dos diagonales
+
+        if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+            return True
+        
+        if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+            return True
+        
+        # Si no se cumplió nada de lo de arriba, devolvemos False
+        return False
+    
+
+    # Selecciona la piedra en (i,j) si pertenece al jugador actual. Si la selección es válida devuelve True, sino False
+    # Esta función se utiliza en move_st para que la funcion sepa qué piedra exacta modificar
+
+    def select_st(i,j):
+        
+        nonlocal selected_index             # La variable se debe actualizar para todas las funciones internas
+
+        # Miramos si las coordenadas que ha puesto están dentro del tablero
+
+        if i < 0 or i >= BSIZ or j < 0 or j >= BSIZ:
+            return False
+        
+        # Asignamos a valor lo que hay en la posición del tablero para hacer las comparaciones
+        valor = board[j][i]
+
+        # Si no hay piedra
+        if valor == NO_PLAYER:
+            return False
+        
+        # Si hay piedra pero es del otro jugador
+        if valor != current_player:
+            return False
+        
+
+        k = 0                                           # Inicializamos el índice que recorre la lista de piedras
+        encontrada = False
+        dim = len(stones_list)
+
+        while k < dim and encontrada == False:          # Mientras k sea menor a la dimensión de la lista y la piedra no se encuentre
+            stone = stones_list[k]
+            
+            if stone.x == i and stone.y == j:           # Buscamos en qué índice de la lista está la piedra seleccionada
+                encontrada = True
+            else:
+                k += 1
+
+        if encontrada == False:
+            return encontrada
+        
+        selected_index = k
+        return True
+
---
+++ logs.md
@@ -0,0 +1,15 @@
+En este archivo aparecerá lo que cada participante del proyecto ha realizado.
+
+
+Logs Cezar Valentin Ivan:
+
+[2/12/2025 12:30]: Lectura del enunciado y creación de logs.md
+[2/12/2025 12:50]: Creación abs_board.py
+[2/12/2025 13:30]: Imports, inicialización de la matriz BSIZ* BSIZ de NO_PLAYER y comentarios
+[2/12/2025 13:45]: Definición y comentarios de las variables involucradas en el tablero
+[2/12/2025 13:45]: Implementación de la función draw_txt()
+[3/12/2025 12:25]: Implementación de la función end()
+[3/12/2025: 13.10]: Implementación de select_st()
+[3/12/2025: 13:45]: Fase de placing de move_st(). Así, cada uno habrá hecho aprox. la mitad del trabajo.
+
+
